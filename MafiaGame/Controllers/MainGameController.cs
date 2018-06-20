ffe4ed0de9cda04ace17cbc414740f40a08ec463
Jelmer Schuiteboer <jelmer.schuiteboer@ndcmediagroep.nl>
2018-06-20 13:04:21 +0200

    Added services & controllers

---
+++ MafiaGame/Controllers/MainGameController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MafiaGame.Controllers
+{
+    public class MainGameController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
