7c0fe91d21a8f21d0a505a4430b187bb174a3bba
Jelmer Schuiteboer <jelmer.schuiteboer@ndcmediagroep.nl>
2018-07-29 14:14:03 +0200

    moved all assets into Assets folder

--- MafiaGame/App_Start/BundleConfig.cs
+++ MafiaGame/App_Start/BundleConfig.cs
@@ -9,36 +9,23 @@ namespace MafiaGame
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            RegisterVendorScripts(bundles);
-            RegisterVendorStyles(bundles);
+            bundles.Add(new ScriptBundle("~/scripts").Include(
+                "~/Assets/Scripts/Tile.js",
+                "~/Assets/Scripts/script.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/script").Include(
-                "~/Scripts/Tile.js",
-                "~/Scripts/script.js"));
-        }
+            bundles.Add(new StyleBundle("~/styles").Include(
+                "~/Assets/Styles/site.css"));
 
-        private static void RegisterVendorScripts(BundleCollection bundles)
-        {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/vendor/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/scripts/vendor").Include(
+                // Use the development version of Modernizr to develop with and learn from. Then, when you're
+                // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+                "~/Assets/Scripts/vendor/modernizr-*",
+                "~/Assets/Scripts/vendor/jquery-{version}.js",
+                "~/Assets/Scripts/vendor/jquery.validate*",
+                "~/Assets/Scripts/vendor/bootstrap.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                "~/Scripts/vendor/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                "~/Scripts/vendor/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/vendor/bootstrap.js"));
-        }
-
-        private static void RegisterVendorStyles(BundleCollection bundles)
-        {
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/bootstrap.css",
-                "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/styles/vendor").Include(
+                "~/Assets/Styles/vendor/bootstrap.css"));
         }
     }
 }
