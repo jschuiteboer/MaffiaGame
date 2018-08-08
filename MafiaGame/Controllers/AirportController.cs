470f6d9ed4b102a2681e0dec6795f88a0bdb8555
Jelmer Schuiteboer <jelmer.schuiteboer@ndcmediagroep.nl>
2018-08-08 11:47:20 +0200

    Added travel functionality

--- MafiaGame/Controllers/AirportController.cs
+++ MafiaGame/Controllers/AirportController.cs
@@ -1,19 +1,56 @@
-﻿using System.Web.Mvc;
+﻿using MafiaGame.Models;
+using MafiaGame.Services;
+using MafiaGame.ViewModels;
+using System.Collections.Generic;
+using System.Web.Mvc;
 
 namespace MafiaGame.Controllers.Activities
 {
     public class AirportController : Controller
     {
+        private IPlayerService _playerService;
+        private ICityService _cityService;
+
+        public AirportController(IPlayerService playerService, ICityService cityService)
+        {
+            this._playerService = playerService;
+            this._cityService = cityService;
+        }
+
         public ActionResult Travel()
         {
-            return View();
+            return View(new AirportViewModel {
+                Cities = this.GetCityList(),
+            });
         }
         
         [HttpPost]
-        public ActionResult Travel(string location)
+        public ActionResult Travel(AirportViewModel model)
         {
+            if (!this.ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+            {
+                PlayerEntity player = this._playerService.GetCurrent();
+                player.City = this._cityService.GetCityFromName(model.SelectedCity);
 
-            return View();
+                return RedirectToAction("", "MainGame");
+            }
+        }
+
+        private List<SelectListItem> GetCityList()
+        {
+            var startingCities = new List<SelectListItem>();
+            var cityNames = this._cityService.GetCityNames();
+
+            foreach (var name in cityNames)
+            {
+                startingCities.Add(new SelectListItem { Text = name });
+            }
+
+            return startingCities;
         }
     }
 }
---
+++ MafiaGame/ViewModels/AirportViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace MafiaGame.ViewModels
+{
+    public class AirportViewModel
+    {
+        public IEnumerable<SelectListItem> Cities { get; set; }
+
+        [Required]
+        public string SelectedCity { get; set; }
+    }
+}
